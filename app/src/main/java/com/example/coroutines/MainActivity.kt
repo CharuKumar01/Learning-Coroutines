404e71fcb4384181bb3bd84b8cab375408f4ee6e
CharuKumar01 <kumarcharu83@gmail.com>
2025-04-03 22:57:29 +0530

    removing unnecesary code

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -8,10 +8,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import com.example.coroutines.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -27,12 +25,5 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        CoroutineScope(Dispatchers.IO).launch {
-            check()
-        }
-    }
-
-    private fun check(){
-        Log.d("check", "Running on thread: ${Thread.currentThread().name}")
     }
 }
