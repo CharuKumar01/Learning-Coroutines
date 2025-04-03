558cc4e7555b2d7472e0150988b7ce4c13dd2869
CharuKumar01 <kumarcharu83@gmail.com>
2025-04-03 22:40:24 +0530

    starting with coroutines

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.coroutines
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import com.example.coroutines.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bind: ActivityMainBinding
@@ -21,5 +27,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        CoroutineScope(Dispatchers.IO).launch {
+            check()
+        }
+    }
+
+    private fun check(){
+        Log.d("check", "Running on thread: ${Thread.currentThread().name}")
     }
 }
