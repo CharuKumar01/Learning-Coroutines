362c3273e2b633fccc72c6e96839a46a6b4cb383
CharuKumar01 <kumarcharu83@gmail.com>
2025-04-04 22:16:22 +0530

    here we goooooooooooooooooo!

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.example.coroutines
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import com.example.coroutines.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bind: ActivityMainBinding
@@ -26,4 +22,5 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+    //here we goooooooooooooooooo!
 }
